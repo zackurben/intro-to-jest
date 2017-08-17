7e8ee4b7947c617fe26a4112c91ee60f7f03525b
Zack Urben <zackurben@gmail.com>
2017-08-16 22:11:41 -0500

    Fixing lint script, adding initial code and some simple tests.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 'use strict';
+
+module.exports = {
+  sum: (...args) => args.filter(Number).reduce((sum, value) => sum + value, 0)
+};
---
+++ index.test.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const pkg = require('./');
+const sum = pkg.sum;
+
+test('sum without arguments returns 0', () => {
+  expect(sum()).toBe(0);
+});
+
+test('sum of 1 and 2 is 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
