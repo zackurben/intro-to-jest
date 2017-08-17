a5ced15368d1ce6c4500999e7216ce0bfcbc11a7
Zack Urben <zackurben@gmail.com>
2017-08-17 17:34:02 -0500

    Adding new sum functions and tests.

--- index.js
+++ index.js
@@ -1,5 +1,44 @@
 'use strict';
 
+/**
+ * Get the sum of the input variables, coerced into a number.
+ * 
+ * @param {*} args
+ *   The arguments to sum.
+ * 
+ * @returns {Number}
+ *   The sum of the input variables or 0.
+ */
+const sum = (...args) => args.filter(Number).reduce((sum, value) => sum + value, 0);
+
+/**
+ * Get the sum of the input variables, coerced into a number, as a delayed promise.
+ * 
+ * @param {*} args
+ *   The arguments to sum.
+ * 
+ * @returns {Promise}
+ *   The promise of the sum.
+ */
+const sumP = (...args) => new Promise(resolve => setTimeout(() => resolve(sum.apply(this, args)), 1000));
+
+/**
+ * Get the sum of the input variables, coerced into a number, as a callback value.
+ * 
+ * @param {*} args
+ *   The arguments to sum with a callback.
+ */
+const sumC = (...args) => {
+  const cb = args.pop();
+  if (!cb || typeof cb !== 'function') {
+    return undefined;
+  }
+
+  return cb(sum.apply(this, args));
+};
+
 module.exports = {
-  sum: (...args) => args.filter(Number).reduce((sum, value) => sum + value, 0)
+  sum,
+  sumP,
+  sumC
 };
--- index.test.js
+++ index.test.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const sum = require('./').sum;
+const { sum, sumP, sumC } = require('./');
 
 test('sum without arguments returns 0', () => {
   expect(sum()).toBe(0);
@@ -8,4 +8,27 @@ test('sum without arguments returns 0', () => {
 
 test('sum of 1 and 2 is 3', () => {
   expect(sum(1, 2)).toBe(3);
+});
+
+test('sumP returns a promise', () => {
+  expect(sumP(1, 2)).toBeInstanceOf(Promise);
+});
+
+test('sumP of 3 and 4 is 7', () =>
+  sumP(3, 4).then(val => {
+    expect(val).toBe(7);
+  }));
+
+test('sumC without a arguments returns undefined', () => {
+  expect(sumC()).toBeUndefined();
+});
+
+test('sumC without a callback returns undefined', () => {
+  expect(sumC(1)).toBeUndefined();
+});
+
+test('sumC of 1 with a callback returns 1', () => {
+  sumC(1, value => {
+    expect(value).toBe(1);
+  });
 });
